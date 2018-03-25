1e76d827b7a1f924fd99905b58b0a4055818b242
C.D. Clark III <clifton.clark@gmail.com>
2018-03-24 20:36:57 -0500

    preparing for packaging.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+import version
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    # long_description = f.read()
+long_description= ""
+
+print("version:",version.__version__)
+setup(
+    name='macro_expander',
+    version=version.__version__,
+    description='Expand macros within a text string. Somewhere between formatting and templating.',
+    long_description=long_description,  # Optional
+    url='https://github.com/CD3/macro_expander',
+    author='C.D. Clark III',
+    packages=["macro_expander"],
+    z
+)
---
+++ util-scripts/tag-release.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+tag=$1
+shift
+
+if [[ -z $tag ]]
+then
+  echo "ERROR: version number required"
+  echo "example: $0 v1.0.1"
+  echo "current tags:"
+  git tag
+  exit 1
+fi
+
+function exit_on_error()
+{
+  echo "There was an error. Commit will not be tagged."
+}
+
+trap exit_on_error ERR
+
+set -e
+root=$(git rev-parse --show-toplevel)
+
+echo "cd'ing to root directory ($root)"
+cd $root
+
+echo "looking for pre-tag-release.sh to run"
+script=$(find ./ -name 'pre-tag-release.sh')
+if [[ -n $script ]]
+then
+  ${script} "${tag}"
+fi
+
+echo "tagging with ${tag}"
+git tag ${tag}
+git tag | grep ${tag}
+echo "Successfully tagged commit."
