7dc4c7944c1015203a36f791e4119d176b51d5a0
CD Clark III <clifton.clark@gmail.com>
2024-09-10 06:40:02 -0500

    chore: cleanup test files

--- macros.py
+++
@@ -1,8 +0,0 @@
-
-def example(self,args,opts):
-  return "Processed by user-defined macro."
-def exampletwo(self,args,opts):
-  return "Processed by user-defined macro 2."
-def otherexample(args,opts):
-  return "Processed by other user-defined macro."
-  
